0335288774e21398c288d7c962d95eceea148a27
Marcos-JG <mgarcia-2024412@kinal.edu.gt>
2025-09-12 15:34:34 -0600

    Primer commit

---
+++ src/main/java/org/Algorix/TallerKinal/TallerKinalApplication.java
@@ -0,0 +1,13 @@
+package org.Algorix.TallerKinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerKinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerKinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/Algorix/TallerKinal/TallerKinalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Algorix.TallerKinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerKinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
