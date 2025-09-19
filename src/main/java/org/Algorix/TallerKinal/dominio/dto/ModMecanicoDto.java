0e588fe8d0d4d0ccc319b93496bd85bd09e7bc1e
Marcos-JG <mgarcia-2024412@kinal.edu.gt>
2025-09-19 16:42:23 -0600

    Se agrego validaciones faltantes a algunos mod

--- src/main/java/org/Algorix/TallerKinal/dominio/dto/ModMecanicoDto.java
+++ src/main/java/org/Algorix/TallerKinal/dominio/dto/ModMecanicoDto.java
@@ -1,8 +1,13 @@
 package org.Algorix.TallerKinal.dominio.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ModMecanicoDto(
+    @NotBlank(message = "El nombre no puede  estar vacio")
     String name,
+    @NotBlank(message = "El apellido no puede estar vacio")
     String lastName,
+    @NotBlank(message = "El teléfono no puede estar vacio")
     String phone
 ) {
 }
